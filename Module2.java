c1af4d699055af92d342427c326e2b941de8517f
Ritika Rauthan <83857624+ritika21022001@users.noreply.github.com>
2022-03-12 09:13:09 +0530

    Create Module2.java

---
+++ Module2.java
@@ -0,0 +1,55 @@
+public class module2 {
+
+    public void chemistry(){
+        System.out.println("");
+
+        String chem[]={"Organic chemistry","Physical chemistry","Bio chemistry","Inorganic chemistry","Polymer chemistry"};
+        System.out.println("The books available in chemistry department are as follow:");
+        for(int i=0; i<chem.length;i++){
+
+            System.out.println(chem[i]);
+
+
+
+        }
+
+
+        }
+
+
+
+        public void physics(){
+
+            String phy[]={"Mechanics","Dynamics","Solid state physics","Astro physics","electromagnetism"};
+            System.out.println("The books available in physics department are as follow:");
+
+            for(int j=0; j<phy.length;j++){
+                System.out.println(phy[j]);
+
+
+            }
+
+        }
+
+
+
+        public void biology(){
+            System.out.println("");
+            String bio[]={"Zoology","Botany","Anatomy","Medicine","Anesthestic"};
+            System.out.println("The books available in biology department are as follow:");
+            for(int k=0;k< bio.length;k++){
+
+                System.out.println(bio[k]);
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+    
+
